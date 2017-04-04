a0e92453d4234bd01e8ce4575cca48b1b036d4a1
Jeffrey Dwyer <Jeffrey Dwyer>
2017-04-03 20:58:27 -0700

    Working on parsing cases

--- cleaning/src/CleanData.hs
+++ cleaning/src/CleanData.hs
@@ -68,7 +68,7 @@ opinionsStart :: T.Text -> Bool
 opinionsStart = (==) "CASES ADJUDGED"
 
 parseTOC :: [T.Text] ->  ([T.Text], [T.Text])
-parseTOC = first toc . span (not . opinionsStart) . drop 1 . dropWhile (not . opinionsStart)
+parseTOC = first (nub . toc) . span (not . opinionsStart) . drop 1 . dropWhile (not . opinionsStart)
     where toc = fmap fst . sortOn snd . rights
               . fmap (R.eitherResult . R.parse tableOfContents)
               
@@ -82,16 +82,41 @@ opinionTitle = parse $ do
     R.try R.space
     entityName
 
-endOfCase = parse $ R.string "It is so ordered." >> R.many' R.anyChar
+-- A line reading "It is so ordered."
+endOfCase = parse $ R.try (R.string "It is so ordered.") <|> R.string "Affirmed." >> R.many' R.anyChar
 
 opinionHeading caseName = parse $  R.many1 R.digit >>  R.space >>  R.string caseName
 
-opinion :: ST.State [T.Text] [T.Text]
-opinion = do
-    ST.modify (dropWhile (not . opinionTitle))
+joiningJustices = do
+        R.string "with whom Justice "
+        R.many1 R.letter
+        R.space
+        R.option ' ' $ do
+            R.string "and Justice"
+            R.many1 R.letter
+            R.space
+        R.try (R.string "joins ") <|> R.string "join "
+
+additionalOpinion opinionType = do
+    R.string "Justice "
+    lastName <- R.many1 R.letter
+    R.string ","
+    R.space
+    R.option " " $ joiningJustices
+    R.string opinionType
+    return lastName
+
+-- | Parses a dissent begining
+--   eg. Scalia, J., dissenting
+--       Justice Stevens, with whom Justice Blackmun and Justice O'Connor join, dissenting.
+dissent = additionalOpinion "dissenting."
+
+opinion :: T.Text -> [[T.Text]] -> ST.State [T.Text] [[T.Text]]
+opinion opinionName cases = do
+    ST.modify (dropWhile (not . opinionTitle)) -- (opinionName `T.isPrefixOf`) . formatOpinionName))
     (c, file) <- span (not . endOfCase) <$> ST.get
     ST.put file
-    return $ c
+    return $ c:cases
 
 removeCaseHeaders :: [T.Text] -> [T.Text]
 removeCaseHeaders [] = []
@@ -109,8 +134,14 @@ cleanData = do
     let filePath = "../data/502.txt"
     print $ "Cleaning " ++ filePath
     (toc, cases) <- parseTOC . T.lines . T.fromStrict <$> TIO.readFile filePath
-    mapM_ print . take 5 $ toc
-    --mapM_ (TIO.putStrLn . T.toStrict) $ cleanCase $ ST.evalState opinion cases
+    --mapM_ print . take 5 $ toc
+    -- Take the toc and fold it using the cases
+    --print $ take 10 cases
+    print $ take 5 toc 
+    let f = foldr1 (>=>) $ fmap opinion $ take 10 toc
+    let g [] = ""
+        g (h:t) = h
+    mapM_ (TIO.putStrLn . T.toStrict . g) $ ST.evalState (f []) cases
       
     
     
